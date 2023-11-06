a06a11370175fc969f7dac1dc51641ed29942ca4
Kiramuun <deimonlight@live.fr>
2023-11-06 14:27:20 +0100

    Update BulletCollisions.cs

--- Assets/BulletCollisions.cs
+++ Assets/BulletCollisions.cs
@@ -5,13 +5,7 @@ using UnityEngine.Rendering;
 
 public class BulletCollisions : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    
     void Update()
     {
         
